776a9aafb540d079829f10fde8efebfb21b0d064
Eric Gilmore <ericgilmore4215@gmail.com>
2022-09-06 12:35:03 -0500

    dsfa

--- avg_steps.py
+++ avg_steps.py
@@ -1,13 +1,11 @@
 import csv
 
 infile = open('steps.csv', 'r')
-outfile = open('avg_steps.csv', 'w', newline = '')
+csvfile = csv.reader(infile, delimiter=',')
+outfile = open('avg_steps.csv','w')
 
-reader = csv.reader(infile, delimiter = ',')
-
-next(reader)
-
-writer = csv.writer(outfile, delimiter = ',')
+next(csvfile)
+outfile.write('Month, Average Steps\n')
 
 jan_total = 0
 jan_count = 0
@@ -32,5 +30,74 @@ oct_count = 0
 nov_total = 0
 nov_count = 0
 dec_total = 0
-dev_count = 0
+dec_count = 0
 
+for row in csvfile: 
+    if int(row[0]) == 1:
+        jan_total += int(row[1])
+        jan_count += 1
+    elif int(row[0]) == 2:
+        feb_total += int(row[1])
+        feb_count += 1
+    elif int(row[0]) == 3:
+        mar_total += int(row[1])
+        mar_count += 1 
+    elif int(row[0]) == 4:
+        april_total += int(row[1])
+        april_count += 1
+    elif int(row[0]) == 5:
+        may_total += int(row[1])
+        may_count += 1
+    elif int(row[0]) == 6:
+        june_total += int(row[1])
+        june_count += 1 
+    elif int(row[0]) == 7:
+        july_total += int(row[1])
+        july_count += 1
+    elif int(row[0]) == 8:
+        aug_total += int(row[1])
+        aug_total += 1
+    elif int(row[0]) == 9:
+        sep_total += int(row[1])
+        sep_count += 1
+    elif int(row[0]) == 10:
+        oct_total += int(row[1])
+        oct_count += 1
+    elif int(row[0]) == 11:
+        nov_total += int(row[1])
+        nov_count += 1
+    elif int(row[0]) == 12:
+        dec_total += int(row[1])
+        dec_count += 1
+
+    JanAvg = jan_total/jan_count
+    FebAvg = feb_total/feb_count
+    MarchAvg = mar_total/mar_count
+    AprilAvg = april_total/april_count
+    MayAvg = may_total/may_count
+    JuneAvg = june_total/june_count
+    JulyAvg = july_total/july_count
+    AugAvg = aug_total/aug_count
+    SepAvg = sep_total/sep_count
+    OctAvg = oct_total/oct_count
+    NovAvg = nov_total/nov_count
+    DecAvg = dec_total/dec_count
+
+
+
+    outfile.writerow('\nJanuary, ' + str(format(JanAvg), '.2f'))
+    outfile.writerow('\nFebuary, ' + str(format(FebAvg), '.2f'))
+    outfile.writerow('\nMarch, ' + str(format(MarchAvg), '.2f'))
+    outfile.writerow('\nApril, ' + str(format(AprilAvg), '.2f'))
+    outfile.writerow('\nMay, ' + str(format(MayAvg), '.2f'))
+    outfile.writerow('\nJune, ' + str(format(JuneAvg), '.2f'))
+    outfile.writerow('\nJuly, ' + str(format(JulyAvg), '.2f'))
+    outfile.writerow('\nAugust, ' + str(format(AugAvg), '.2f'))
+    outfile.writerow('\nSeptember, ' + str(format(SepAvg), '.2f'))
+    outfile.writerow('\nOctober, ' + str(format(OctAvg), '.2f'))
+    outfile.writerow('\nNovember, ' + str(format(NovAvg), '.2f'))
+    outfile.writerow('\nDecember, ' + str(format(DecAvg), '.2f'))
+
+
+    outfile.close() 
+        
