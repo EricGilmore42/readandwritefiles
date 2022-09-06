3075a1f08176d66a4ed3ffb1280b0a42155629f5
Eric Gilmore <ericgilmore4215@gmail.com>
2022-09-06 10:59:18 -0500

    dsfg

--- avg_steps.py
+++ avg_steps.py
@@ -9,15 +9,28 @@ next(reader)
 
 writer = csv.writer(outfile, delimiter = ',')
 
-
-count = '0'
-
-for row in infile: 
-    if row[0] == '1':
-        writer.writerow('January')
-        while row[0] == '1':
-            count += row[1]
-        writer.writerow(count)
-    
-outfile.close()
+jan_total = 0
+jan_count = 0
+feb_total = 0
+feb_count = 0 
+mar_total = 0
+mar_count = 0
+april_total = 0
+april_count = 0
+may_total = 0
+may_count = 0
+june_total = 0
+june_count = 0
+july_total = 0
+july_count = 0
+aug_total = 0
+aug_count = 0
+sep_total = 0
+sep_count = 0
+oct_total = 0
+oct_count = 0
+nov_total = 0
+nov_count = 0
+dec_total = 0
+dev_count = 0
 
